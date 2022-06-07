4c425f640a9518718c53f634876d1794b162df42
Mark Williams <markwilliams1977@gmail.com>
2022-06-07 09:14:24 -0500

    Section 13, 14, 15, 16: Mapping an array of objects

--- src/App.js
+++ src/App.js
@@ -7,16 +7,45 @@ import NewEntryForm from "./components/NewEntryForm";
 import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
 import EntryLine from "./components/EntryLine";
+import {useState} from "react";
+
+const initialEntries = [
+    {
+        description:'Work income',
+        value:'$1000,00',
+        isExpense:false
+    },
+    {
+        description:'Water bill',
+        value:'$20,00',
+        isExpense:true
+    },
+    {
+        description:'Rent',
+        value:'$300,00',
+        isExpense:true
+    }
+];
 
 function App() {
+    const [entries, setEntries] = useState(initialEntries)
   return (
     <Container>
         <MainHeader title='Budget' type='h1'/>
         <DisplayBalance size='small' label='Your Balance' value='2,267.53'/>
+
         <DisplayBalances columns='2' textAlign='center' divided />
         <MainHeader title='History' type='h3' />
-        <EntryLine description='Income' amount='$10.00'/>
-        <EntryLine description='Expense' amount='$10.00' isExpense />
+
+        {entries.map((entry) => (
+            <EntryLine
+            description={entry.description}
+            amount={entry.value}
+            isExpense={entry.isExpense}
+            />
+            ))}
+
+
         <MainHeader title='Add new transaction' type='h3' />
         <NewEntryForm />
     </Container>
