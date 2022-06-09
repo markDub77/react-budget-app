d07fdedf0a1c6bcb56576c8073d5a299eae90628
Mark Williams <markwilliams1977@gmail.com>
2022-06-09 11:28:26 -0500

    Section 22: Creating a model to edit the entries

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import {Grid, GridColumn, GridRow, Icon, Segment} from "semantic-ui-react";
+import React, {Fragment, useState} from 'react';
+import { Grid, GridColumn, GridRow, Icon, Segment } from "semantic-ui-react";
+import ModalEdit from "./ModalEdit";
 
-const EntryLine = ({id ,description, value, isExpense = false, deleteEntry}) => (
-    <Segment color={isExpense ? 'red' : 'green'}>
-        <Grid columns={3} textAlign='right'>
-            <GridRow>
-                <GridColumn width={10} textAlign='left'>{description}</GridColumn>
-                <GridColumn width={3} textAlign='right'>{value}</GridColumn>
-                <GridColumn width={3}>
-                    <Icon name='edit' bordered/>
-                    <Icon name='trash' bordered onClick={() => deleteEntry(id)}/>
-                </GridColumn>
-            </GridRow>
-        </Grid>
-    </Segment>
-);
+const EntryLine = ({id ,description, value, isExpense = false, deleteEntry}) => {
+    const [isOpen, setIsOpen] = useState(false)
+    return (<Fragment>
+            <Segment color={isExpense ? 'red' : 'green'}>
+                <Grid columns={3} textAlign='right'>
+                    <GridRow>
+                        <GridColumn width={10} textAlign='left'>{description}</GridColumn>
+                        <GridColumn width={3} textAlign='right'>{value}</GridColumn>
+                        <GridColumn width={3}>
+                            <Icon name='edit' bordered onClick={() => setIsOpen(true)} />
+                            <Icon name='trash' bordered onClick={() => deleteEntry(id)} />
+                        </GridColumn>
+                    </GridRow>
+                </Grid>
+            </Segment>
+            <ModalEdit isOpen={isOpen}/>
+        </Fragment>
+    )
+};
 
 export default EntryLine;
---
+++ src/components/ModalEdit.js
@@ -0,0 +1,11 @@
+import {Modal} from "semantic-ui-react";
+
+const ModalEdit = ({isOpen}) => {
+    return (
+        <Modal open={isOpen}>
+
+        </Modal>
+    )
+}
+
+export default ModalEdit
