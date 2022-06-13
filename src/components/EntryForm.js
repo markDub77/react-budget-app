fbd5926006955cd148729137fc075d0f25498497
Mark Williams <markwilliams1977@gmail.com>
2022-06-13 14:19:14 -0500

    Section 24: Making our components more reusable

---
+++ src/components/EntryForm.js
@@ -0,0 +1,43 @@
+import React, {Fragment} from 'react';
+import {Checkbox, FormGroup, FormInput, Segment} from "semantic-ui-react";
+import ButtonSaveOrCancel from "./ButtonSaveOrCancel";
+
+const EntryForm = ({description, value, isExpense, setDescription, setValue, setIsExpense, addEntry}) => (
+    <Fragment>
+        <FormGroup widths={3}>
+            <FormInput
+                icon='tags'
+                width={12}
+                label='Description'
+                placeholder='New shinny thing'
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />
+            <FormInput
+                width={4}
+                label='value'
+                placeholder='100.00'
+                icon='dollar'
+                iconPosition='left'
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+            ></FormInput>
+            <ButtonSaveOrCancel
+                addEntry={addEntry}
+                description={description}
+                value={value}
+                isExpense={isExpense}
+            />
+        </FormGroup>
+        <Segment compact>
+            <Checkbox
+                toggle
+                label='is expense'
+                checked={isExpense}
+                onChange={() => setIsExpense((oldState) => !oldState)}
+            />
+        </Segment>
+    </Fragment>
+);
+
+export default EntryForm;
--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {Checkbox, Form, FormGroup, FormInput, Segment} from "semantic-ui-react";
-import ButtonSaveOrCancel from "./ButtonSaveOrCancel";
+import { Form } from "semantic-ui-react";
+import EntryForm from "./EntryForm";
 
 function NewEntryForm({addEntry}) {
     const [description, setDescription] = useState('')
@@ -8,34 +8,15 @@ function NewEntryForm({addEntry}) {
     const [isExpense, setIsExpense] = useState(true)
     return (
         <Form unstackable>
-            <FormGroup>
-                <FormInput
-                    icon='tags'
-                    width={12}
-                    label='Description'
-                    placeholder='New shinny thing'
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
-                />
-                <FormInput
-                    width={4}
-                    label='value'
-                    placeholder='100.00'
-                    icon='dollar'
-                    iconPosition='left'
-                    value={value}
-                    onChange={(event) => setValue(event.target.value)}
-                ></FormInput>
-                <ButtonSaveOrCancel addEntry={addEntry} description={description} value={value} isExpense={isExpense} />
-            </FormGroup>
-            <Segment compact>
-                <Checkbox
-                    toggle
-                    label='is expense'
-                    checked={isExpense}
-                    onChange={() => setIsExpense((oldState) => !oldState)}
-                />
-            </Segment>
+            <EntryForm
+                description={description}
+                value={value}
+                isExpense={isExpense}
+                setValue={setValue}
+                setDescription={setDescription}
+                setIsExpence={setIsExpense}
+                addEntry={addEntry}
+            />
         </Form>
     )
 }
