15a69dcadeeedf361dfcead043731bda43a47de2
Mark Williams <markwilliams1977@gmail.com>
2022-06-27 09:09:41 -0500

    Section 43: Adding a new entry with React Redux Hooks

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,9 +1,29 @@
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
 import { Form } from "semantic-ui-react";
-import EntryForm from "./EntryForm";
 import ButtonSaveOrCancel from "./ButtonSaveOrCancel";
-import React from "react";
+import EntryForm from "./EntryForm";
+import {addEntryRedux} from "../actions/entries.actions";
 
-function NewEntryForm({addEntry, description, value, setValue, setDescription, setIsExpense, isExpense}) {
+function NewEntryForm() {
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+    const [isExpense, setIsExpense] = useState(true)
+    const dispatch = useDispatch()
+
+    function addEntry() {
+        dispatch(addEntryRedux({
+            id: 5,
+            description,
+            value,
+            isExpense
+        }))
+        setDescription('')
+        setValue('')
+        setIsExpense(true)
+    }
+
+
     return (
         <Form unstackable>
             <EntryForm
