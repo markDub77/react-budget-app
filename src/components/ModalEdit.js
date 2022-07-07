a38b8f1a0a75a1e9d1365cb6d0dc96461e6df6be
Mark Williams <markwilliams1977@gmail.com>
2022-07-07 12:40:59 -0500

    Section 52: Closing the modal on dispatch

--- src/components/ModalEdit.js
+++ src/components/ModalEdit.js
@@ -1,5 +1,7 @@
 import {Button, Modal, ModalActions, ModalContent, ModalHeader} from "semantic-ui-react";
 import EntryForm from "./EntryForm";
+import {useDispatch} from "react-redux";
+import {closeEditModal} from "../actions/modals.actions";
 
 const ModalEdit = ({
     isOpen,
@@ -11,6 +13,7 @@ const ModalEdit = ({
     setIsExpense,
     isExpense
 }) => {
+    const dispatch = useDispatch();
     return (
         <Modal open={isOpen}>
             <ModalHeader>Edit entry</ModalHeader>
@@ -25,7 +28,7 @@ const ModalEdit = ({
                 />
             </ModalContent>
             <ModalActions>
-                <Button onClick={() => setIsOpen(false)}>Close</Button>
+                <Button onClick={() => dispatch(closeEditModal())}>Close</Button>
                 <Button onClick={() => setIsOpen(false)} primary>Ok</Button>
             </ModalActions>
         </Modal>
--- src/reducers/modals.reducers.js
+++ src/reducers/modals.reducers.js
@@ -3,6 +3,7 @@ const modalsReducers = (state = {}, action) => {
         case 'OPEN_EDIT_MODAL':
             return {...state, isOpen: true, id: action.payload.id}
         case 'CLOSE_EDIT_MODAL':
+            return {...state, isOpen: false, id: null}
         default:
             return state
     }
